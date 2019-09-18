072d69d3d8817b48e8c590d5a11b3ef15bb9f511
kybDev <kyben.developer@gmail.com>
2019-09-18 11:24:18 +0800

    Update Work Card view

--- src/container/Works/ProjectCard/ProjectCard.js
+++ src/container/Works/ProjectCard/ProjectCard.js
@@ -6,21 +6,33 @@ const ProjectCard = (props) => {
 
       <div className="card-title">
         <div className="proj-name">
-          <h3>{ props.name }</h3>
-          <a href={ props.url }>{ props.url }</a>
+          <h3>{props.name}</h3>
+          <a href={props.url}>{props.url}</a>
         </div>
       </div>
 
       <div className="card-body">
         <div className="proj-info">
-          <p>{ props.description }</p>
+          <p>{props.description}</p>
         </div>
         <div className="tech-tags">
+          <div className="dev-info">
+            <div className="dev-info-desc">
+              Company : <span>Medisource Healthcare Systems Inc.</span>
+            </div>
+            <div className="dev-info-desc">
+              Development Position : <span>Full Stack</span>
+            </div>
+          </div>
+
+
+
+
           <h5>Technology Stack</h5>
           <ul>
             {
               props.tags.map((tag, key) => {
-                return <li key={ key }>{ tag }</li>;
+                return <li key={key}>{tag}</li>;
               })
             }
           </ul>
